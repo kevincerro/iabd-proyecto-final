7025c3e059afc9638ac97c5f5b8ca2765b25a2c0
Kevin <kevincerro1997@gmail.com>
2022-05-28 13:07:56 +0200

    Improve project structure

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return '<p>Hello, World!</p>'
+# Import controllers
+from src.controller import homepage
+app.register_blueprint(homepage.mod)
 
 
 @app.errorhandler(404)
@@ -16,3 +15,4 @@ def error_not_found(e):
 @app.errorhandler(500)
 def error_not_found(e):
     return '<p>500 Internal Server Error</p>'
+
