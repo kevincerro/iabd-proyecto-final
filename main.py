37983bdb8f5443c3a98dc75068da1ce9d3e2cdb3
Kevin <kevincerro1997@gmail.com>
2022-05-31 19:14:25 +0200

    Trust CloudFront and APIGateway headers

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
+# Initialize flask
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_host=1)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
