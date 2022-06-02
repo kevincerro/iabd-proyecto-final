077d88141c0684fe8fe098f70ff46a887317422d
Kevin <kevincerro1997@gmail.com>
2022-06-02 22:41:59 +0200

    Allow to upload speech files to s3

--- src/controller/dashboard.py
+++ src/controller/dashboard.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, logout_user, current_user
 from main import db
 from src.entity.text_to_speech import TextToSpeech
+from src.form.speech_to_text_form import SpeechToTextForm
 from src.form.text_to_speech_form import TextToSpeechForm
 from src.service import aws_service
 
@@ -35,12 +36,12 @@ def new_text_to_speech():
         file_name = aws_service.text_to_speech(text)
 
         # Store in db
-        user = TextToSpeech(
+        tts = TextToSpeech(
             text=text,
             speech=file_name,
             created_by=current_user.id
         )
-        db.session.add(user)
+        db.session.add(tts)
         db.session.commit()
 
         return redirect(url_for('dashboard.text_to_speech'))
@@ -51,7 +52,36 @@ def new_text_to_speech():
 @mod.route('/speech-to-text')
 @login_required
 def speech_to_text():
-    return render_template('dashboard/text-to-speech/list.twig')
+    return render_template('dashboard/speech-to-text/list.twig')
+
+
+@mod.route('/speech-to-text/new', methods=["GET", "POST"])
+@login_required
+def new_speech_to_text():
+    form = SpeechToTextForm()
+
+    if form.validate_on_submit():
+        # Read form data
+        file_name = form.file_name.data
+        file_mime_type = form.file_mime_type.data
+
+        print(file_name, file_mime_type)
+
+        # Process with AWS Polly
+        # file_name = aws_service.text_to_speech(text)
+
+        # Store in db
+        # stt = SpeechToText(
+        #     speech=file_name,
+        #     text=text,
+        #     created_by=current_user.id
+        # )
+        # db.session.add(stt)
+        # db.session.commit()
+
+        return redirect(url_for('dashboard.speech_to_text'))
+
+    return render_template('dashboard/speech-to-text/new-edit.twig', form=form)
 
 
 @mod.route('/logout')
--- src/service/aws_service.py
+++ src/service/aws_service.py
@@ -1,9 +1,12 @@
 import os
 import tempfile
+import time
+
 from boto3 import Session
 
 session = Session()
 polly = session.client('polly')
+transcribe = session.client('transcribe')
 s3 = session.client('s3')
 s3_bucket: str = os.getenv('UPLOADS_BUCKET')
 
@@ -22,10 +25,30 @@ def text_to_speech(text):
     )
 
     # Store audio in S3
-    return upload_text_to_speech_to_s3(response['AudioStream'].read())
+    return upload_speech_to_s3(response['AudioStream'].read())
 
 
-def upload_text_to_speech_to_s3(blob):
+def speech_to_text(file_url):
+    transcribe.start_transcription_job(
+        TranscriptionJobName=file_url,
+        Media={'MediaFileUri': file_url},
+        MediaFormat='mp3',
+        LanguageCode='es-ES'
+    )
+
+    max_tries = 10
+    while max_tries > 0:
+        max_tries -= 1
+        job = transcribe.get_transcription_job(TranscriptionJobName=file_url)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+        if job_status == 'COMPLETED':
+            return job['TranscriptionJob']['Transcript']['TranscriptFileUri']
+        time.sleep(1)
+
+    raise Exception('Cannot convert speech-to-text')
+
+
+def upload_speech_to_s3(blob):
     with tempfile.NamedTemporaryFile(suffix='.mp3') as t:
         file_name = os.path.basename(t.name)
         s3.put_object(
@@ -43,6 +66,22 @@ def get_presigned_speech_url(speech):
         Params={
             'Bucket': s3_bucket,
             'Key': 'text_to_speech/' + speech
+        },
+        ExpiresIn=3600
+    )
+
+
+def generate_random_file_name():
+    with tempfile.NamedTemporaryFile() as file_name:
+        return os.path.basename(file_name.name)
+
+
+def get_presigned_upload_speech_url(file_name):
+    return s3.generate_presigned_url(
+        'put_object',
+        Params={
+            'Bucket': s3_bucket,
+            'Key': 'tmp_uploads/' + file_name
         },
         ExpiresIn=3600
     )
